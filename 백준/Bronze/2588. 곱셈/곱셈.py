c81c71f10da978acef2b0c267ef8fed8f434f618
MyeongSeok98 <103018497+MyeongSeok98@users.noreply.github.com>
2025-03-14 13:47:07 +0900

    [Bronze III] Title: 곱셈, Time: 32 ms, Memory: 32412 KB -BaekjoonHub

--- 백준/Bronze/2588. 곱셈/곱셈.py
+++ 백준/Bronze/2588. 곱셈/곱셈.py
@@ -1,7 +1,7 @@
 a = int(input())
-b = int(input())
+b = input()
 
-print(a * (b%10))
-print(a * (b%100 - b% 10) // 10)
-print(a * (b - b%100 ) // 100)
-print(a * b)
+for  i in range(2, -1, -1):
+    print(a * int(b[i]))
+
+print(a * int(b))
