95182ee0644238fb32145827ef3005a980227d09
MyeongSeok98 <103018497+MyeongSeok98@users.noreply.github.com>
2025-03-19 20:52:35 +0900

    [Bronze III] Title: 별 찍기 - 5, Time: 36 ms, Memory: 32412 KB -BaekjoonHub

---
+++ 백준/Bronze/2442. 별 찍기 － 5/별 찍기 － 5.py
@@ -0,0 +1,10 @@
+import sys
+
+input = sys.stdin.readline
+
+if __name__== '__main__':
+    N = int(input())
+
+    for i in range(N):
+        print(" " * (N-i-1),end="")
+        print("*" * ((2 * i)+1))
