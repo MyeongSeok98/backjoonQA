cd5718383fa93ee644bc66c203c191ffd21f29fb
MyeongSeok98 <103018497+MyeongSeok98@users.noreply.github.com>
2025-03-20 12:47:51 +0900

    [Bronze I] Title: 더하기 사이클, Time: 40 ms, Memory: 32412 KB -BaekjoonHub

--- 백준/Bronze/1110. 더하기 사이클/더하기 사이클.py
+++ 백준/Bronze/1110. 더하기 사이클/더하기 사이클.py
@@ -1,28 +1,22 @@
-import sys 
-input = sys.stdin.readline
+import sys
 
-if __name__ == '__main__':
-    N = input()             
-    first = N
-    a = ''
-    
-    if(N[1] != '\n'):
-        a = int(N[0])+int(N[1])
-        result = N[1] + str(a%10)
-    else:
-        a = N[0]
-        result = N[0] + str(a)
-        
-    cnt = 1
-    while(int(first)!=int(result)):
-        cnt+=1
+N=int(sys.stdin.readline())
+count=1
 
-        a = int(result[0]) + int(result[1])
-        if(a >= 10):
-            result = result[1] + str(a)[1]
-        else:    
-            result = result[1] + str(a)[0]
-        
+def set(n):
+    if n<10:
+        a=(n*10)//10
+        b=(n*10)%10
+        return (n*10)+((a+b)%10)
+    else :
+        a=n//10
+        b=n%10
+        return (b*10)+((a+b)%10)
 
-    print(cnt)
-        
+tmep=set(N)
+
+while tmep != N:
+    tmep=set(tmep)
+    count+=1
+     
+print(count)    
