47a424f573f66d769cf3d4488324f362d7d5dd91
MyeongSeok98 <103018497+MyeongSeok98@users.noreply.github.com>
2025-03-20 11:17:28 +0900

    [Silver III] Title: 1, 2, 3 더하기, Time: 32 ms, Memory: 32412 KB -BaekjoonHub

--- 백준/Silver/9095. 1， 2， 3 더하기/1， 2， 3 더하기.py
+++ 백준/Silver/9095. 1， 2， 3 더하기/1， 2， 3 더하기.py
@@ -2,20 +2,26 @@ import sys
 input = sys.stdin.readline
 cnt = 0
 def func(t: int, finalScore: int):
+    ## 만약 점수를 넘어가버리면 폐기
     if(t > finalScore):
         return
+    ## 목표숫자랑 같으면 cnt+1하고 리턴하자
     if(t==finalScore):
         global cnt
         cnt+=1
         return
     else:
+        ## 1을 더하고 넘어가던가
         func(t+1, finalScore)
+        ## 2를 더하고 넘어가던가
         func(t+2, finalScore)
+        ## 3을 더하고 넘어가던가
         func(t+3, finalScore)
 
 if __name__ == '__main__':
     T = int(input())
     for cases in range(T):        
+        ## cnt는 미리 초기화 시키기
         cnt = 0
         N = int(input())
         func(0, N)
