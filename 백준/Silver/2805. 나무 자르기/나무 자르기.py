10dbd59e3ffa361c17076f2db365ea371c4ff96b
MyeongSeok98 <103018497+MyeongSeok98@users.noreply.github.com>
2025-03-22 00:15:55 +0900

    [Silver II] Title: 나무 자르기, Time: 568 ms, Memory: 263068 KB -BaekjoonHub

--- 백준/Silver/2805. 나무 자르기/나무 자르기.py
+++ 백준/Silver/2805. 나무 자르기/나무 자르기.py
@@ -1,26 +1,43 @@
 import sys
+from typing import MutableSequence
 input = sys.stdin.readline
 
-def find_max_cutting_height(forest, needTreeMin):
-    minHeight, maxHeight = 0, max(forest)
-    maxCuttingHeight = 0
-
-    while minHeight <= maxHeight:
-        middleHeight = (minHeight + maxHeight) // 2
-        timberTree = sum(max(0, tree - middleHeight) for tree in forest)
-
-        if timberTree == needTreeMin:
-            return middleHeight  # 정답이므로 즉시 반환
-        elif timberTree > needTreeMin:
-            maxCuttingHeight = middleHeight  # 가능한 최댓값 업데이트
-            minHeight = middleHeight + 1
+needTreeMin = 0
+maxCuttingHeight = -1
+def func(forestArr : list, minHeight : int, maxHeight):
+    # 종료조건
+    if minHeight > maxHeight:
+        return 
+    timberTree = 0
+    middleHeight = (maxHeight + minHeight) // 2
+    
+    
+    for i in range(0, len(forestArr)):
+        if forestArr[i] < middleHeight:
+            continue
         else:
-            maxHeight = middleHeight - 1
-
-    return maxCuttingHeight  # 가능한 최댓값 반환
+            timberTree += forestArr[i] - middleHeight
+    if timberTree == needTreeMin:
+        print(middleHeight)
+        exit(0)
+        return
+    else:
+        if timberTree < needTreeMin:
+            maxHeight = middleHeight-1
+        if timberTree > needTreeMin:
+            global maxCuttingHeight
+            maxCuttingHeight = max(maxCuttingHeight, middleHeight)
+            minHeight = middleHeight+1
+        func(forestArr, minHeight, maxHeight)
+        
+            
 
 if __name__ == '__main__':
     treeEntity, needTreeMin = map(int, input().split())
-    forest = list(map(int, input().split()))
-    
-    print(find_max_cutting_height(forest, needTreeMin))
+    forest = list(map(int,input().split()))
+    forest.sort()
+    maxHeight = forest[treeEntity-1]
+    minHeight = 0
+    func(forest, minHeight, maxHeight)
+
+    print(maxCuttingHeight)
