6b18205a52ba69805e9ce57e06af9d77ef4086d6
MyeongSeok98 <103018497+MyeongSeok98@users.noreply.github.com>
2025-03-22 16:43:12 +0900

    [Gold IV] Title: 사냥꾼, Time: 844 ms, Memory: 50744 KB, Score: 100 point -BaekjoonHub

--- 백준/Gold/8983. 사냥꾼/사냥꾼.py
+++ 백준/Gold/8983. 사냥꾼/사냥꾼.py
@@ -14,39 +14,39 @@ if __name__ == '__main__':
         if fireRange < animalY:
             continue
         animalXY.append([animalX, animalY])
-
+    
+    #사격 위치 정렬
     firePlaceX.sort()
-    animalXY.sort()
-    # print(f"사격 위치들 X좌표 : {firePlaceX}")
-    # print(f"사정거리 : {fireRange}")
-    # print(f"노루들 [X,Y]좌표 : {animalXY}")
 
-    ## animal[i][0] = i번째 사냥감의 x좌표
-    ## animal[i][1] = i번째 사냥감의 y좌표
     for i in range(len(animalXY)):
         start = 0
         last = firePlaceNum-1
-        sameX = False
         closeX = 0
+
+        ## 만약 노루위치가 제일 왼쪽 사격위치보다 더 왼쪽인 경우
         if animalXY[i][0] < firePlaceX[0]:
             closeX = firePlaceX[0]
+        ## 만약 노루위치가 제일 오른쪽 사격위치보다 더 오른쪽인 경우
         elif animalXY[i][0] > firePlaceX[firePlaceNum-1]:
             closeX = firePlaceX[firePlaceNum-1]
+        ## 노루위치가 범위안에 있는 경우
         else:
             while(start <= last):
                 middle = (start+last) // 2
-                if firePlaceX[middle] == animalXY[i][0]:
-                    break
-                if firePlaceX[middle] > animalXY[i][0]:
+                if firePlaceX[middle] >= animalXY[i][0]:
                     last = middle-1
                 if firePlaceX[middle] < animalXY[i][0]:
                     start = middle+1
-            # print(f"{animalXY[i]}일 때, {firePlaceX[start]}, {firePlaceX[last]}")
-            if abs(animalXY[i][0] - firePlaceX[start]) < abs(animalXY[i][0] - firePlaceX[last]):
+            # start가 범위를 벗어나버린 경우
+            if start >= firePlaceNum:
+                closeX = firePlaceX[last]
+            # last가 범위를 벗어나버린 경우
+            elif last < 0:
+                closeX = firePlaceX[start]
+            elif abs(animalXY[i][0] - firePlaceX[start]) < abs(animalXY[i][0] - firePlaceX[last]):
                 closeX = firePlaceX[start]
             else:
                 closeX = firePlaceX[last]
-            # print(f"{animalXY[i]}일 때, 근삿값은 {closeX}")
 
         if abs(closeX - animalXY[i][0]) + animalXY[i][1] <= fireRange:
             killCount+=1
@@ -54,17 +54,9 @@ if __name__ == '__main__':
         
     print(killCount)
                 
-
-
-
-
-
 ## 첫번째 사대부터 사정거리 안의 동물을 죽이고
 ## 다음 사대도 사정거리 안의 동물을 죽이고
 ## 그다음 사대도 사정거리 안의 동물을 죽이고
 # .....
 ## 마지막 사대까지 사정거리 안의 동물을 죽이고
-## 킬카운트를 출력
-
-## 큰 이등변 삼각형을 두개의 이등변삼각형으로 나눔
 
