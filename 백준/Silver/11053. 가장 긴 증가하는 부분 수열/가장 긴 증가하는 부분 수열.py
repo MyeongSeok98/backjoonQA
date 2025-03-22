cec696cf68e2a34a07413f0e3e1462826eb3e106
MyeongSeok98 <103018497+MyeongSeok98@users.noreply.github.com>
2025-03-22 11:42:40 +0900

    [Silver II] Title: 가장 긴 증가하는 부분 수열, Time: 36 ms, Memory: 32412 KB -BaekjoonHub

--- 백준/Silver/11053. 가장 긴 증가하는 부분 수열/가장 긴 증가하는 부분 수열.py
+++ 백준/Silver/11053. 가장 긴 증가하는 부분 수열/가장 긴 증가하는 부분 수열.py
@@ -1,22 +1,32 @@
+import sys
+
+input = sys.stdin.readline
+
+
 if __name__ == "__main__":
     N = int(input())
     A = list(map(int, input().split()))
 
     arr = [A[0]]
-    
-    for i in range(1, N):
-        if A[i] < arr[0]:
-            arr[0] = A[i]  
-        elif A[i] > arr[-1]:
+    cnt = 0
+    for i in range(1,N):
+        if(A[i] < arr[0]):
+            arr[0] = A[i]
+            continue
+        if(A[i] > arr[cnt]):
             arr.append(A[i])
-        else:
-            first, end = 0, len(arr) - 1
-            while first <= end:
-                middle = (first + end) // 2
-                if arr[middle] >= A[i]:
-                    end = middle - 1
-                else:
-                    first = middle + 1
-            arr[first] = A[i]  # **🚨 이 부분이 중요!**
-    
-    print(len(arr))  # LIS의 길이 출력
+            cnt+=1
+            continue
+        first= 0
+        end = cnt
+        while(first <= end):
+            middle = (first + end) // 2
+            if(arr[middle] >= A[i]):
+                end = middle-1
+            if(arr[middle] < A[i]):
+                first = middle+1
+        arr[first] = A[i]
+    print(cnt+1)
+
+
+        
