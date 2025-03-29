f26225c727489b9df0a41126a20698c8e2a98a60
MyeongSeok98 <103018497+MyeongSeok98@users.noreply.github.com>
2025-03-29 09:56:05 +0900

    [Gold IV] Title: 이진 검색 트리, Time: 72 ms, Memory: 36104 KB -BaekjoonHub

--- 백준/Gold/5639. 이진 검색 트리/이진 검색 트리.py
+++ 백준/Gold/5639. 이진 검색 트리/이진 검색 트리.py
@@ -1,28 +1,30 @@
 import sys
-
-sys.setrecursionlimit(10**6)  # 깊은 재귀 허용
+sys.setrecursionlimit(10**6)
 input = sys.stdin.read
-
-def preToPost(startIdx: int, endIdx: int):
+tree = []
+def preToPost(startIdx: int, endIdx:int):
     if startIdx > endIdx:
         return
-
     rootIdx = startIdx
-    resultIdx = endIdx + 1  # 오른쪽 서브트리 시작점 (초기값: 존재하지 않으면 endIdx+1)
 
-    # root보다 큰 첫 번째 원소 찾기 → 오른쪽 서브트리의 시작점
-    for i in range(startIdx + 1, endIdx + 1):
-        if tree[i] > tree[rootIdx]:
-            resultIdx = i
-            break
-
-    # 왼쪽 서브트리 처리
-    preToPost(startIdx + 1, resultIdx - 1)
-    # 오른쪽 서브트리 처리
-    preToPost(resultIdx, endIdx)
-    # 현재 root 출력
+    resultIdx = endIdx + 1
+    backupStartIdx = startIdx
+    backupEndIdx = endIdx
+    startIdx+=1
+    while startIdx <= endIdx:
+        midIdx = (startIdx + endIdx)//2
+        if tree[midIdx] <= tree[rootIdx]:
+            startIdx = midIdx + 1
+        elif tree[midIdx] > tree[rootIdx]:
+            resultIdx = midIdx
+            endIdx = midIdx - 1
+    if backupStartIdx <= resultIdx - 1:        
+        preToPost(backupStartIdx+1, resultIdx-1)
+    if resultIdx < len(tree):
+        preToPost(resultIdx, backupEndIdx)
     print(tree[rootIdx])
 
-if __name__ == "__main__":
+if __name__ == '__main__':
     tree = list(map(int, input().split()))
-    preToPost(0, len(tree) - 1)
+
+    preToPost(0, len(tree)-1)
