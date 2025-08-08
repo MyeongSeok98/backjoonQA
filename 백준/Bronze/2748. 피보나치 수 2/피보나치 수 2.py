3f51c0ae518efd0de5794a382e81b3982db144d9
MyeongSeok98 <103018497+MyeongSeok98@users.noreply.github.com>
2025-08-08 23:24:48 +0900

    [Bronze I] Title: 피보나치 수 2, Time: 44 ms, Memory: 32412 KB -BaekjoonHub

--- 백준/Bronze/2748. 피보나치 수 2/피보나치 수 2.py
+++ 백준/Bronze/2748. 피보나치 수 2/피보나치 수 2.py
@@ -1,27 +1,12 @@
-import sys
+a = [0 for _ in range(1000)]
 
-input = sys.stdin.readline
-
-a = [0 for _ in range(1000005)]
-n = 0
-def func(num : int)-> int:
-    if num == 0:
-        a[num] = 0
-        return 0
-    if num == 1:
-        a[num] = 1
-        return 1
-    
-    if a[num] > 0:
-        return  a[num]
-    else:
-        a[num] = func(num-1)+func(num-2)
-        return a[num]
-
+a[0] = 0
+a[1] = 1
+a[2] = 1
 
 if __name__ =="__main__":
-    
     n = int(input())
-    
-    func(n)
+    if(n >= 3):
+        for i in range(3, n+1):
+            a[i] = a[i-1] + a[i-2]
     print(a[n])
