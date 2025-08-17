9f0cd2539c4b1f30359b084d21b6d8df2b5e63e1
MyeongSeok98 <103018497+MyeongSeok98@users.noreply.github.com>
2025-08-18 01:29:05 +0900

    [level 0] Title: 이어 붙인 수, Time: 0.07 ms, Memory: 87.4 MB -BaekjoonHub

---
+++ 프로그래머스/0/181928. 이어 붙인 수/이어 붙인 수.java
@@ -0,0 +1,17 @@
+class Solution {
+    public int solution(int[] num_list) {
+        StringBuilder sb1 = new StringBuilder();
+        StringBuilder sb2 = new StringBuilder();
+        for(int i = 0; i < num_list.length; i++){
+            if(num_list[i] % 2 == 0){
+                sb1.append(num_list[i]);
+            }
+            else{
+                sb2.append(num_list[i]);
+            }
+        }
+        
+        int answer = Integer.parseInt(sb1.toString()) + Integer.parseInt(sb2.toString());
+        return answer;
+    }
+}
